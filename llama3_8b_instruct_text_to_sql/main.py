045d206730935d5547be91c46c57737c84514f03
Emad Dehnavi <dehnavi@gmx.com>
2024-08-23 16:51:15 +0200

    prompt generator helper function

--- llama3_8b_instruct_text_to_sql/main.py
+++ llama3_8b_instruct_text_to_sql/main.py
@@ -8,16 +8,26 @@ lamini.api_key = lamini_api_key
 llm = lamini.Lamini(model_name="meta-llama/Meta-Llama-3-8B-Instruct")
 
 def start():
-    prompt = ( 
-    "<|begin_of_text|>"  # Start of prompt
-    "<|start_header_id|>system<|end_header_id|>\n\n"  #  header - system
-    "You are a helpful assistant."  # system prompt
-    "<|eot_id|>" # end of turn
-    "<|start_header_id|>user<|end_header_id|>\n\n" # header - user
-    "Please write a birthday card for my good friend Andrew" 
-    "<|eot_id|>" # end of turn
-    "<|start_header_id|>assistant<|end_header_id|>\n\n" # header - assistant
-    )
+    system_prompt = user_prompt = "You are a helpful assistant."
+    user_prompt = "Please write a birthday card for my good friend Andrew"
+    prompt = make_llama_3_prompt(user_prompt, system_prompt)
     print(prompt)
     result = llm.generate(prompt, max_new_tokens=200)
     print(result)
+
+
+def make_llama_3_prompt(user, system=""):
+    system_prompt = ""
+    if system != "":
+        system_prompt = (
+            f"<|start_header_id|>system<|end_header_id|>\n\n{system}"  #  header - system prompt
+            f"<|eot_id|>" # end of turn
+        )
+    prompt = (
+        f"<|begin_of_text|>{system_prompt}" # Start of prompt
+        f"<|start_header_id|>user<|end_header_id|>\n\n" # start header - user
+        f"{user}"
+        f"|eot_id|" # end of turn
+        f"<|start_header_id|>assistant<|end_header|>\n\n" # header - assistant
+    )
+    return prompt
